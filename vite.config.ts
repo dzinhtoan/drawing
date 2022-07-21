9e986dbbbe10c0b8b148b7ecbd3b1ad1092ac883
DESKTOP-0IH2RMP\dzinhtoan <dzinhtoan@gmail.com>
2022-07-21 22:39:46 +0700

    [THREEJS] - try deploying

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
           '@': path.resolve(__dirname, './src'),
         },
       },
-      base: env.PROD ? '/drawing' : '/'
+      base: env.PROD ? '/drawing/' : '/'
   }
   return config;
 })
