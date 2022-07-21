acfd204ed5120c2a16db146cbd8faaedc055d4dc
DESKTOP-0IH2RMP\dzinhtoan <dzinhtoan@gmail.com>
2022-07-21 22:56:15 +0700

    [THREEJS] - try deploying

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,14 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig(({ command, mode, ssrBuild }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  let config = {
-    plugins: [vue()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, './src'),
-        },
-      },
-      base: env.PROD ? '/drawing/' : '/'
-  }
-  return config;
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: '/drawing/'
 })
