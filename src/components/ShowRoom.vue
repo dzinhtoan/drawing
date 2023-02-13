e9fe6fd53e2ec8a0e1ba59af73acff7023dd2184
LAPTOP-P8232EVV\OLLI <toan.bui@olli-ai.com>
2023-02-13 09:39:20 +0700

    [Drawing] - more options

--- src/components/ShowRoom.vue
+++ src/components/ShowRoom.vue
@@ -11,13 +11,13 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
-// import Sample1CubeVue from './Sample1Cube.vue';
-// import Sample2LineVue from './Sample2Line.vue';
-// import Sample3Sphere from './Sample3Sphere.vue';
+import Sample1CubeVue from './Sample1Cube.vue';
+import Sample2LineVue from './Sample2Line.vue';
+import Sample3Sphere from './Sample3Sphere.vue';
 import Sample4SphereMove from './Sample4SphereMove.vue';
-// import Sample5TextureVue from './Sample5Texture.vue';
-// import Sample6ModelingVue from './Sample6Modeling.vue';
-// // import Sample7EarthVue from './Sample7Earth.vue';
+import Sample5TextureVue from './Sample5Texture.vue';
+import Sample6ModelingVue from './Sample6Modeling.vue';
+// import Sample7EarthVue from './Sample7Earth.vue';
 // import Sample8BlackOlliVue from './Sample8BlackOlli.vue';
 // import Sample9GridOlli from './Sample9GridOlli.vue';
 
@@ -26,26 +26,26 @@ export default defineComponent({
     name: '',
     setup() {
         const productList = [
-            // { 
-            //     name: 'Simple Cube',
-            //     component: Sample1CubeVue,
-            // },
+            { 
+                name: 'Simple Cube',
+                component: Sample1CubeVue,
+            },
             // { 
             //     name: 'Simple Line',
             //     component: Sample2LineVue,
             // },
-            // { 
-            //     name: 'Simple Sphere',
-            //     component: Sample3Sphere,
-            // },
+            { 
+                name: 'Simple Sphere',
+                component: Sample3Sphere,
+            },
             { 
                 name: 'Simple Controls',
                 component: Sample4SphereMove
             },
-            // { 
-            //     name: 'Simple Texture',
-            //     component: Sample5TextureVue
-            // },
+            { 
+                name: 'Simple Texture',
+                component: Sample5TextureVue
+            },
             // { 
             //     name: 'Simple Modeling',
             //     component: Sample6ModelingVue
